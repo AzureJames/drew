dbc0afbd25a0a25cfef0796c6079868e20d7488c
Azure James G.M <77216235+AzureJames@users.noreply.github.com>
2022-05-21 12:55:14 -0600

    subtle background shift

---
+++ main.js
@@ -0,0 +1,9 @@
+let whereIworked = document.querySelector(".whereihaveworked");
+let maxHeight = document.documentElement.scrollHeight;
+let theOpacity = window.scrollY/maxHeight;
+
+document.addEventListener("scroll", function(e) { //darkening background color on scroll
+    theOpacity = (window.scrollY/maxHeight)-.69;
+    whereIworked.style.backgroundColor = "rgba(0,0,0,"+theOpacity+")";
+    console.log(theOpacity);
+});
