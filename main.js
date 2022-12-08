a8ca7ab9153c787ac08a4053eee4ebfccca32f58
Azure James G.M <77216235+AzureJames@users.noreply.github.com>
2022-12-07 20:32:52 -0700

    dec7 2022

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ let maxHeight = document.documentElement.scrollHeight;
 let theOpacity = window.scrollY/maxHeight;
 
 document.addEventListener("scroll", function(e) { //darkening background color on scroll
-    theOpacity = (window.scrollY/maxHeight)-.69;
+    theOpacity = (window.scrollY/maxHeight)-.45;
     whereIworked.style.backgroundColor = "rgba(0,0,0,"+theOpacity+")";
-    console.log(theOpacity);
 });
